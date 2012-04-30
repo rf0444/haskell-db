a6ac82371fd7be63fdaf0cbe4c6c309a63239f4c
rf <rf0444@gmail.com>
2012-04-30 16:54:22 +0900

    より単純なケースに変更

--- mongo/Model.hs
+++ mongo/Model.hs
@@ -7,19 +7,6 @@ import Database.Persist.MongoDB
 import Database.Persist.TH
 import Language.Haskell.TH.Syntax
 
--- You can define all of your database entities in the entities file.
--- You can find more information on persistent and how to declare entities
--- at:
--- http://www.yesodweb.com/book/persistent/
 share [mkPersist MkPersistSettings { mpsBackend = ConT ''Action }, mkMigrate "migrateAll"]
     $(persistFileWith lowerCaseSettings "config/models")
-
-data Sex = Male | Female
-  deriving (Show, Read, Eq, Ord, Enum, Bounded)
-
-data Attend = Suspense | Absent | Present
-  deriving (Show, Read, Eq, Ord, Enum, Bounded)
-
-derivePersistField "Sex"
-derivePersistField "Attend"
 
--- mongo/main.hs
+++ mongo/main.hs
@@ -1,16 +1,13 @@
 {-# LANGUAGE TypeFamilies, QuasiQuotes, MultiParamTypeClasses, TemplateHaskell, OverloadedStrings #-}
 
-import Database.Persist
 import Database.Persist.MongoDB
 
 import Model
 
 main = withMongoDBConn dbname hostname $ runMongoDBConn master $ do
-  insert $ UserAuth {
-    userAuthEmail = "hoge@hoge.jp",
-    userAuthPassword = Just "hoge",
-    userAuthVerkey = Just "hoge",
-    userAuthVerified = True
+  insert $ User {
+    userEmail = "hoge@hoge.jp",
+    userPassword = Just "hoge"
   }
   return ()
  where
